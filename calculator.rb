a7f3936e1f999b482a25d56af14fcf569b2ccbe2
Oscar A Castro <oacastro2@gmail.com>
2020-08-17 21:50:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 12 
 second_number = 1 
-13 = first_number + second_number
+sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number \ second_number
